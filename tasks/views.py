270d85cfad676243e78070f50c527fbd0f886bc0
tiagodelima-sc <tiago.l07@aluno.ifsc.edu.br>
2022-02-13 13:06:51 -0300

    incluindo a função adicionar e criando os forms

--- tasks/views.py
+++ tasks/views.py
@@ -1,18 +1,32 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from .forms import TaskForm
 
 from .models import Task
 # Create your views here.
 
 def taskList(request):
     #Pegando todo os objetos do BD
-    tasks = Task.objects.all()
+    tasks = Task.objects.all().order_by('-create_at')
     #Colocando as tarefas no template, os valores que resgatei no BD
     return render(request, 'tasks/list.html', {'tasks': tasks})
 
 def taskView(request, id):
     task = get_object_or_404(Task, pk=id)
     return render(request, 'tasks/task.html', {'task': task})
+
+def newTask(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.done = 'doing'
+            task.save()
+            return redirect('/')
+    else:
+        form = TaskForm()
+        return render(request, 'tasks/addtask.html',  {'form': form})
 
 def helloWorld(request):
     return HTTPResponse('Hello World')
